99e3f78a4064b807eb8c3808e9056e90aade2071
Abolfazl-Amani <abolfazl.amani.cs84@gmail.com>
2026-06-12 10:37:53 +0330

    Write a method to create a queue

--- queue.c
+++ queue.c
@@ -12,6 +12,21 @@ typedef struct Queue {
     int size;
 } Queue;
 
+Queue* create_queue() {
+    Queue* queue = (Queue*) malloc(sizeof(Queue));
+
+    if(queue == NULL) {
+        printf("Memory Allocation Encountered with an Error!");
+        return 1;
+    }
+    
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->size = 0;
+
+    return queue;
+}
+
 int main() {
 
 
